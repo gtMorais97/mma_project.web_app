1b4d402bf59be947964fef8a155070007bb06fcb
gtMorais97 <gustavo.t.morais@gmail.com>
2022-02-20 00:17:26 +0000

    responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,20 @@ module.exports = {
   theme: {
     extend: {},
     theme: {
-      fontFamily: {
-        sans: ['Arial', 'SFMono-Regular']
-      }
+      '2xl': { 'max': '1535px' },
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': { 'max': '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': { 'max': '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      'md': { 'max': '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      'sm': { 'max': '639px' },
+      // => @media (max-width: 639px) { ... }
     },
   },
   plugins: [],
