a25f601908fd636781e41817ca380de17b605f4b
gtMorais97 <gustavo.t.morais@gmail.com>
2022-02-26 16:46:23 +0000

    responsive phone size

--- components/RankList.js
+++ components/RankList.js
@@ -4,11 +4,11 @@ import { Rank } from "./Rank";
 
 export function RankList({ ranks, medians, maxes }) {
     return (
-        <div className="grid content-center grid-cols-1 max-w-screen-lg">
+        <div className="grid content-center grid-cols-1 max-w-screen-lg lg:divide-y-2">
             {
                 Object.keys(ranks).map(rankId => {
                     return (
-                        <div className=" grid grid-cols-2 lg:grid-cols-1 md:grid-cols-1 items-center">
+                        <div className=" grid grid-cols-2 lg:grid-cols-1 md:grid-cols-1 lg:pb-3 lg:pt-2  items-center">
                             <Rank key={rankId} rank={ranks[rankId]} />
                             <BarChart rank={ranks[rankId]} median={medians.find(m => m.id === rankId)} max={maxes[rankId]} />
                         </div>
