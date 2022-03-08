d67c2b151e699a3cebcadd798b2a98875fddaf66
gtMorais97 <gustavo.t.morais@gmail.com>
2022-03-08 23:54:34 +0000

    Component refactors

---
+++ components/Drop.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Dropdown } from 'rsuite';
+
+export function Drop({
+    setRankSelector,
+    currentRanks,
+    totalsFilter,
+    currentFilter
+}) {
+    return <div className=' flex justify-center font-mono text-white'>
+        <Dropdown title="Select Rank" onSelect={eventKey => {
+            setRankSelector(eventKey);
+        }}>
+            <Dropdown.Item eventKey={undefined}>All</Dropdown.Item>
+            {currentRanks.hasOwnProperty(totalsFilter) ? Object.keys(currentRanks[currentFilter]).map(rankId => <Dropdown.Item eventKey={rankId}>{rankId}</Dropdown.Item>) : Object.keys(currentRanks).map(rankId => <Dropdown.Item eventKey={rankId}>{rankId}</Dropdown.Item>)}
+        </Dropdown>
+    </div>;
+}
