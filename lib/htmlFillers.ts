2f03a467ec36aa4bf1f5a3681544a8f35b5941c3
gtMorais97 <gustavo.t.morais@gmail.com>
2022-04-25 19:37:09 +0100

    Added jest testing

--- lib/htmlFillers.ts
+++ lib/htmlFillers.ts
@@ -1,7 +1,17 @@
-export function getOpponentSurname(element) {
+export function getOpponentSurname(element: Object): string {
     return element['opponent_name'] ?
         `(vs ${element['opponent_name'].split(" ").pop()})` : //Only show the last name
         ""
+}
+
+export function getLastName(fullName: string): string {
+    var names = fullName.split(" ");
+    var lastName = "";
+    do {
+        lastName = names.pop()
+    } while (lastName.endsWith('.'))
+
+    return lastName
 }
 
 export async function getFighterTapology(fighterNameAndNickname) {
